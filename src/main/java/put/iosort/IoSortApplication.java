95f26ed5f0f51acb75fbcb1e5cda07fa40216940
Milosz <miloszsobolsci@gmail.com>
2024-11-21 17:54:41 +0100

    init

---
+++ src/main/java/put/iosort/IoSortApplication.java
@@ -0,0 +1,13 @@
+package put.iosort;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IoSortApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IoSortApplication.class, args);
+    }
+
+}
