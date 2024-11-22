8bac111071807d5992a750a356189abb81217dd5
Milosz <miloszsobolsci@gmail.com>
2024-11-22 01:23:47 +0100

    IOS-2 Implemented BubbleSort

--- src/main/java/put/iosort/Service/Strategies/BubbleSort.java
+++ src/main/java/put/iosort/Service/Strategies/BubbleSort.java
@@ -8,9 +8,29 @@ import put.iosort.Service.Strategy.Strategy;
 public class BubbleSort implements Strategy {
     @Override
     public int[] sort(int[] array, Order order, int iterations) {
+        int n = array.length;
+
+        outerloop:
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if ((order == Order.ASC && array[j] > array[j + 1]) ||
+                        (order == Order.DESC && array[j] < array[j + 1])) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+
+                if (i * j == iterations) {
+                    break outerloop;
+                }
+
+
+            }
+        }
+        return array;
+    }
 
 
 
-        return new int[0];
     }
 }
