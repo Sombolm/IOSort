112a66b4b3b188d57a14ca6bdbbd631df4c1bf65
Miłosz Sobol <miloszsobolsci@gmail.com>
2024-12-08 20:39:25 +0100

    commented deprecated static endpoint

--- src/main/java/put/iosort/Config/RestEndpoints.java
+++ src/main/java/put/iosort/Config/RestEndpoints.java
@@ -7,7 +7,7 @@ public class RestEndpoints {
     public static final String GET = "/get";
     public static final String ARRAY = "/array";
     public static final String ORDER = "/{order}";
-    public static final String STRATEGY_TYPE = "/{strategyType}";
+    //public static final String STRATEGY_TYPE = "/{strategyType}";
 
     public static final String ITERATIONS = "/{iterations}";
 }
